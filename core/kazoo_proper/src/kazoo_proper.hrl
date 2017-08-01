eed3149d225070045dccb01caf8db10a2b1f263b
James Aimonetti <james.aimonetti@gmail.com>
2017-08-01 17:17:36 +0000

    logging macros

--- core/kazoo_proper/src/kazoo_proper.hrl
+++ core/kazoo_proper/src/kazoo_proper.hrl
@@ -4,5 +4,30 @@
 
 -define(FAILED_RESPONSE, <<"{}">>).
 
+-define(DEBUG(Fmt)
+       ,data:debug(pqc_log:log_info(), Fmt)
+       ).
+
+-define(DEBUG(Fmt, Args)
+       ,data:debug(pqc_log:log_info(), Fmt, Args)
+       ).
+
+-define(INFO(Fmt)
+       ,data:info(pqc_log:log_info(), Fmt)
+       ).
+
+-define(INFO(Fmt, Args)
+       ,data:info(pqc_log:log_info(), Fmt, Args)
+       ).
+
+-define(ERROR(Fmt)
+       ,data:error(pqc_log:log_info(), Fmt)
+       ).
+
+-define(ERROR(Fmt, Args)
+       ,data:error(pqc_log:log_info(), Fmt, Args)
+       ).
+
+
 -define(KAZOO_PROPER_HRL, 'true').
 -endif.
