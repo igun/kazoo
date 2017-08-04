1da38286c9e4d5c52c2b88343b3ee20943fb0163
James Aimonetti <james.aimonetti@gmail.com>
2017-08-04 21:44:18 +0000

    query available plans, add cleanup

--- core/kazoo_proper/src/pqc_cb_service_plans.erl
+++ core/kazoo_proper/src/pqc_cb_service_plans.erl
@@ -3,6 +3,8 @@
 -export([create_service_plan/2
         ,delete_service_plan/2
         ,assign_service_plan/3
+        ,available_service_plans/2
+        ,cleanup/0
         ]).
 
 -include("kazoo_proper.hrl").
@@ -38,6 +40,22 @@ assign_service_plan(API, AccountId, ServicePlanId) ->
                            ,kz_json:encode(RequestEnvelope)
                            ).
 
+-spec available_service_plans(pqc_cb_api:state(), ne_binary() | proper_types:type()) ->
+                                     pqc_cb_api:response().
+available_service_plans(API, AccountId) ->
+    URL = string:join([account_service_plan_url(AccountId), "available"], "/"),
+    RequestHeaders = pqc_cb_api:request_headers(API),
+    pqc_cb_api:make_request([200]
+                           ,fun kz_http:get/3
+                           ,URL
+                           ,RequestHeaders
+                           ).
+
 -spec account_service_plan_url(ne_binary()) -> string().
 account_service_plan_url(AccountId) ->
     string:join([pqc_cb_accounts:account_url(AccountId), "service_plans"], "/").
+
+-spec cleanup() -> 'ok'.
+cleanup() ->
+    kazoo_services_maintenance:remove_orphaned_services(),
+    kt_cleanup:cleanup_soft_deletes(<<"services">>).
